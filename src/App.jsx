0d0dcf933587ad6d055ef152401f27f3e4a558ff
Matheus Graziani Santana <RES0104491@aluno.firjansenaisesi.com.br>
2025-07-10 20:15:39 -0300

    Finalizando listar-cadastro e corrigindo bug no APP.jsx onde foi criado rotas para locais inexistentes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from './pages/home/page'
+
 import Header from './components/Header/Header'
-import Cadastrar from "./pages/cadastrar/page"
-import EditarCadastro from "./pages/editarcadastro/page"
-import ListarCadastro from "./pages/listar-cadastro/page";
+import Cadastrar from "./pages/cadastrar/cadastrar"
+
+import ListarCadastro from "./pages/listar-cadastro/listar-cadastro";
 import './App.css';
 
 function App() {
@@ -13,10 +13,11 @@ function App() {
     <>
     <Router >
       <Header/>
+     
       <Routes>
-        <Route path="/home" element={<Home/>}/>
+        
         <Route path="/cadastrar" element={<Cadastrar/>}/>
-        <Route path="/editar-cadastro" element={<EditarCadastro/>}/>
+        
         <Route path="/listar-cadastro" element={<ListarCadastro/>}/>
       </Routes>
     </Router>
--- src/pages/listar-cadastro/listar-cadastro.jsx
+++ src/pages/listar-cadastro/listar-cadastro.jsx
@@ -2,39 +2,41 @@ import { useEffect } from 'react'
 import styles from './listar-cadastro.module.css'
 import { useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
+
 
 const ListarCadastro = () => {
   const [usuarios, setUsuarios] = useState([])
 
   const fetchUsuarios = async () => {
-  try {
-    const res = await axios.get('http://localhost:3001/usuarios');
-    setUsuarios(res.data);
-  } catch (err) {
-    console.error('Erro ao buscar usuários:', err);
-  }
-};
-useEffect(() => {
-  fetchUsuarios();
-}, []);
+    try {
+      const res = await axios.get('http://localhost:3001/usuarios');
+      setUsuarios(res.data);
+    } catch (err) {
+      console.error('Erro ao buscar usuários:', err);
+    }
+  };
+  useEffect(() => {
+    fetchUsuarios();
+  }, []);
 
   const excluirUsuario = async (id) => {
-  const confirmar = window.confirm("Tem certeza que deseja excluir?");
-  if (!confirmar) return;
+    const confirmar = window.confirm("Tem certeza que deseja excluir?");
+    if (!confirmar) return;
 
-  try {
-    const res = await axios.delete(`http://localhost:3001/usuarios/${id}`);
-    if (res.status === 204) {
-      alert("Usuário excluído com sucesso!");
-      setUsuarios((prev) => prev.filter((user) => user.id !== id));
-    } else {
+    try {
+      const res = await axios.delete(`http://localhost:3001/usuarios/${id}`);
+      if (res.status === 204) {
+        alert("Usuário excluído com sucesso!");
+        setUsuarios((prev) => prev.filter((user) => user.id !== id));
+      } else {
+        alert("Erro ao excluir usuário.");
+      }
+    } catch (error) {
+      console.error("Erro ao excluir:", error);
       alert("Erro ao excluir usuário.");
     }
-  } catch (error) {
-    console.error("Erro ao excluir:", error);
-    alert("Erro ao excluir usuário.");
-  }
-};
+  };
 
   return (
     <div className={styles.tudo}>
@@ -62,7 +64,9 @@ useEffect(() => {
                 <td>{user.nome}</td>
                 <td>{user.email}</td>
                 <td>
-                  <button className={styles['btn-editar']}>Editar</button>
+                  <Link to="/editar-cadastro">
+                    <button className={styles['btn-editar']}>Editar</button>
+                  </Link>
                   <button className={styles['btn-excluir']} onClick={() => excluirUsuario(user.id)} >Excluir</button>
                 </td>
               </tr>
